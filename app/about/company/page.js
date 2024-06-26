e6e2f3d5bf7f66b9a0e2a35d4064c8bd54a7796a
SachPlayZ <ssachinsingh99@gmail.com>
2024-06-27 00:22:50 +0530

    App Initialized

---
+++ app/about/company/page.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>Arnab Ki maa ki chut</div>
+  )
+}
+
+export default page
---
+++ app/about/page.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>Arnab</div>
+  )
+}
+
+export default page
