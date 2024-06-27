3df214737ae994284e3e9b90d291ab90460258a1
SachPlayZ <ssachinsingh99@gmail.com>
2024-06-28 04:27:51 +0530

    Metadata changes

--- app/layout.js
+++ app/layout.js
@@ -4,6 +4,11 @@ import { cn } from '@/lib/utils'
 import './globals.css'
 import Navbar from './_components/Navbar'
 
+export const metadata = {
+  title: 'FinZ',
+  description: 'A simple expense management app',
+}
+
 const fontHeading = Inter({
   subsets: ['latin'],
   display: 'swap',
