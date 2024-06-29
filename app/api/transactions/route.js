dce688af3a58c3ce1b7bc70ea6f0c2e6e4a7574a
Arnab S <arnabsengupta104@gmail.com>
2024-06-30 03:24:56 +0530

    Integration

---
+++ app/api/transactions/route.js
@@ -0,0 +1,30 @@
+"use server"
+
+import { NextResponse } from 'next/server';
+import connectToDatabase from '../../_middleware/mongodb';
+import mongoose from 'mongoose';
+import { TransactionSchema } from '@/app/_models/schema';
+import { verifyToken } from '@/app/_middleware/auth';
+
+async function posthandler(req) {
+    await connectToDatabase();
+    const token = req.headers.get('Authorization');
+    
+    if (!token) {
+        return NextResponse.json({ message: 'Unauthorized' }, { status: 401 });
+    }
+    const id = await verifyToken(token);
+    if (!id) {
+        return NextResponse.json({ message: 'Unauthorized' }, { status: 401 });
+    }
+    const Transaction = mongoose.models.Transaction || mongoose.model('Transaction', TransactionSchema);
+    const transactions = await Transaction.find({ user: id}).exec();
+
+    if (!transactions) {
+        return NextResponse.json({ message: 'No transactions found' }, { status: 404 });
+    }
+    const sortedTransactions = transactions.sort((a, b) => new Date(b.date) - new Date(a.date));
+    return NextResponse.json(sortedTransactions, { status: 200 });
+}
+
+export { posthandler as POST };
