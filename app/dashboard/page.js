b2d57d66a93e7bfc08aecf33a7f0796b3593fa46
SachPlayZ <ssachinsingh99@gmail.com>
2024-06-29 17:41:42 +0530

    changes

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -101,14 +101,13 @@ export default function Page() {
               </div>
             )}
           </div>
-          <Link
-            href="#"
-            className="text-muted-foreground hover:text-foreground bg-black hover:bg-slate-800 rounded-full"
-            prefetch={false}>
+          <Button
+            className="text-muted-foreground hover:text-foreground bg-black hover:bg-slate-800 rounded-full p-4"
+            >
             <BarChartIcon className="w-6 h-6" />
             <span className="sr-only">Analysis</span>
-          </Link>
-          <Button variant="ghost" size="icon" className="rounded-full">
+          </Button>
+          <Button variant="ghost" size="icon" className="text-muted-foreground hover:text-foreground bg-black hover:bg-slate-800 rounded-full">
             <PlusIcon className="w-6 h-6" />
             <span className="sr-only">Add Transaction</span>
           </Button>
@@ -313,27 +312,6 @@ function BriefcaseIcon(props) {
 }
 
 
-function InfoIcon(props) {
-  return (
-    (<svg
-      {...props}
-      xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round">
-      <circle cx="12" cy="12" r="10" />
-      <path d="M12 16v-4" />
-      <path d="M12 8h.01" />
-    </svg>)
-  );
-}
-
-
 function PlusIcon(props) {
   return (
     (<svg
@@ -343,7 +321,7 @@ function PlusIcon(props) {
       height="24"
       viewBox="0 0 24 24"
       fill="none"
-      stroke="currentColor"
+      stroke="#ea580c"
       strokeWidth="2"
       strokeLinecap="round"
       strokeLinejoin="round">
