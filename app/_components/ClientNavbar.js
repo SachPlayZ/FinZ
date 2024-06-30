5402d532cf304a7c9c6dba3ab033da67d42ca4d9
SachPlayZ <ssachinsingh99@gmail.com>
2024-06-30 11:20:03 +0530

    Small miss

--- app/_components/ClientNavbar.js
+++ app/_components/ClientNavbar.js
@@ -7,7 +7,7 @@ import { usePathname } from 'next/navigation';
 
 export default function ClientNavbar() {
   const pathname = usePathname();
-  if (pathname === '/account' || pathname === '/dashboard' || pathname === 'analytics') {
+  if (pathname === '/account' || pathname === '/dashboard' || pathname === '/analytics') {
     return null;
   }
   return <Navbar />;
